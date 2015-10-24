86e9480ed4c6e41ba039d8fa18303c700e7b2f71
jasony0206 <jasony0206@gmail.com>
2015-10-24 00:33:39 -0700

    Extract scripts to query.js

--- query.js
+++ query.js
@@ -1,3 +1,5 @@
+$(document).foundation();
+
 function getEarthquakeData(magnitude, period) {
   $.ajax({
     url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/" 
@@ -24,6 +26,29 @@ function addEntry(tabName, feature) {
 
   var coordinates = feature.geometry.coordinates;
   pinDown(coordinates[1], coordinates[0]);
+}
+
+function initializeMap() {
+  var mapCanvas = document.getElementById('map');
+  var losAngeles = {lat: 34.05, lng: -118.25};
+  var africa = {lat: 7.18, lng: 21.09};
+  var mapOptions = {
+    center: africa,
+    zoom: 1,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
+  map = new google.maps.Map(mapCanvas, mapOptions)
+
+  getEarthquakeData("1.0", "hour");
+  //getEarthquakeData("4.5", "day");
+}
+
+function pinDown(latitude, longitude) {
+  new google.maps.Marker({
+    position: {lat: latitude, lng: longitude},
+    map: map
+  });
 }
 
 // when click day tab, make a call
